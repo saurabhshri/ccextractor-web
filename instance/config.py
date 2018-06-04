ec97089786cc418e5265e19465c2cada1d35458c
Saurabh Shrivastava <saurabh.shrivastava54@gmail.com>
2018-06-04 12:46:24 +0530

    Try adding MySQL to Travis.

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,4 @@
 #config variables in this file should not reach git, fill it with your own values
 SECRET_KEY = 'THESECRETKEY'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/test'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1/test'
 SECRET_CONFIG_READING_TEST = 'Its a magical place.'
