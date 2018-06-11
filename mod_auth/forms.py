be23601ccf14524b08bbb3c2c49f4b2b1bb48dc0
Saurabh Shrivastava <saurabh.shrivastava54@gmail.com>
2018-06-12 00:07:39 +0530

    Use variables to decide min and max password length.

--- mod_auth/forms.py
+++ mod_auth/forms.py
@@ -16,12 +16,16 @@ from email_validator import validate_email, EmailNotValidError
 
 
 def valid_password(form, field):
+    from flask import current_app as app
+    min_pwd_len = app.config['MIN_PWD_LEN']
+    max_pwd_len = app.config['MAX_PWD_LEN']
     pass_size = len(field.data)
     if pass_size == 0:
         raise ValidationError('The password can not be empty!')
-    if pass_size < 8 or pass_size > 128:
+    if pass_size < min_pwd_len or pass_size > max_pwd_len:
         raise ValidationError(
-            'Password needs to be between 8 and 128 characters long (you entered {char})'.format(char=pass_size)
+            'Password needs to be between {min_pwd_len} and {max_pwd_len} characters long (you entered {char})'.format
+            (min_pwd_len=min_pwd_len, max_pwd_len=max_pwd_len, char=pass_size)
         )
 
 def valid_email(form, field):
