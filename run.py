d93462002e2c8ef71c05caca3b4e3361bb8d7112
Saurabh Shrivastava <saurabh.shrivastava54@gmail.com>
2018-07-25 01:58:27 +0530

    Add a filter to show time since a given time. `x minutes ago`

--- run.py
+++ run.py
@@ -60,6 +60,32 @@ def init_db():
         db.session.add(ccextractor)
         db.session.commit()
 
+@app.template_filter()
+def timesince(dt, default="just now"):
+    """
+    Returns string representing "time since" e.g.
+    3 days ago, 5 hours ago etc.
+    """
+    from datetime import datetime, timezone
+    now = datetime.now(timezone.utc)
+    diff = now - dt
+
+    periods = (
+        (diff.days / 365, "year", "years"),
+        (diff.days / 30, "month", "months"),
+        (diff.days / 7, "week", "weeks"),
+        (diff.days, "day", "days"),
+        (diff.seconds / 3600, "hour", "hours"),
+        (diff.seconds / 60, "minute", "minutes"),
+        (diff.seconds, "second", "seconds"),
+    )
+
+    for period, singular, plural in periods:
+        if period >= 1:
+            return "%d %s ago" % (period, singular if period == 1 else plural)
+
+    return default
+
 @app.before_first_request
 def before_first_request():
     db.create_all()
