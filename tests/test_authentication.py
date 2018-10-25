34b3d9119a7067ea64aa333d74695ac1bd5a201f
Albert <alufers@wp.pl>
2018-10-25 19:52:18 +0200

    Added tests for users and files admin pages (#8)
    * Added tests for users and files admin pages
    
    * Moved tests to the test_authentication.py
    
    * Fixed error with missing import
    
    * FIxed bugs with CSRF and added more specific asserts

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -10,8 +10,10 @@ import unittest
 import time
 from run import app
 from tests.template_test_helper import captured_templates
-
+from database import db
+from mod_auth.models import Users, AccountType
 from mod_auth.controller import generate_verification_code, send_verification_mail, send_signup_confirmation_mail
+
 
 class TestEmailSignUp(unittest.TestCase):
     def setUp(self):
@@ -26,7 +28,7 @@ class TestEmailSignUp(unittest.TestCase):
             assert template.name == 'mod_auth/signup.html'
 
     def test_blank_email(self):
-        response = self.signup_email(email = '')
+        response = self.signup_email(email='')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Email address is not filled in', response.data)
 
@@ -44,18 +46,22 @@ class TestEmailSignUp(unittest.TestCase):
         for email in invalid_email_address:
             response = self.signup_email(email)
             self.assertEqual(response.status_code, 200)
-            self.assertIn(b'Entered value is not a valid email address.', response.data)
+            self.assertIn(
+                b'Entered value is not a valid email address.', response.data)
 
     def test_sending_verification_mail(self):
         with app.app_context():
             email = 'someone@example.com'
             expires = int(time.time()) + 86400
-            verification_code = generate_verification_code("{email}{expires}".format(email=email, expires=expires))
-            response = send_verification_mail(email, verification_code, expires)
+            verification_code = generate_verification_code(
+                "{email}{expires}".format(email=email, expires=expires))
+            response = send_verification_mail(
+                email, verification_code, expires)
             self.assertEqual(response.status_code, 401)
 
     def signup_email(self, email):
         return app.test_client().post('/signup', data=dict(email=email), follow_redirects=True)
+
 
 class TestVerify(unittest.TestCase):
     def setUp(self):
@@ -71,7 +77,8 @@ class TestVerify(unittest.TestCase):
     def test_if_verification_link_expired(self):
         now = int(time.time())
         link_expired_at = now - 3600
-        response = app.test_client().get('/verify/someone@example.com/verificationcode/{expires}'.format(expires=link_expired_at), follow_redirects=True)
+        response = app.test_client().get(
+            '/verify/someone@example.com/verificationcode/{expires}'.format(expires=link_expired_at), follow_redirects=True)
         self.assertIn(b'verification link is expired', response.data)
 
     def test_if_verification_unssuccessul(self):
@@ -79,23 +86,29 @@ class TestVerify(unittest.TestCase):
             email = 'someone@example.com'
             expires = int(time.time()) + 86400
             fake_expire = int(time.time()) + 3600
-            verification_code = generate_verification_code("{email}{expires}".format(email=email, expires=expires))
+            verification_code = generate_verification_code(
+                "{email}{expires}".format(email=email, expires=expires))
 
-            response = app.test_client().get('/verify/{email}/{verification_code}/{expires}'.format(email='wrong@email.com', verification_code=verification_code, expires=expires), follow_redirects=True)
+            response = app.test_client().get('/verify/{email}/{verification_code}/{expires}'.format(
+                email='wrong@email.com', verification_code=verification_code, expires=expires), follow_redirects=True)
             self.assertIn(b'Verification failed!', response.data)
 
-            response = app.test_client().get('/verify/{email}/{verification_code}/{expires}'.format(email=email, verification_code='wrong-code', expires=expires), follow_redirects=True)
+            response = app.test_client().get('/verify/{email}/{verification_code}/{expires}'.format(
+                email=email, verification_code='wrong-code', expires=expires), follow_redirects=True)
             self.assertIn(b'Verification failed!', response.data)
 
-            response = app.test_client().get('/verify/{email}/{verification_code}/{expires}'.format(email=email, verification_code=verification_code, expires=fake_expire), follow_redirects=True)
+            response = app.test_client().get('/verify/{email}/{verification_code}/{expires}'.format(
+                email=email, verification_code=verification_code, expires=fake_expire), follow_redirects=True)
             self.assertIn(b'Verification failed!', response.data)
 
     def test_if_verification_successful(self):
         with app.app_context():
             email = 'someone@example.com'
             expires = int(time.time()) + 86400
-            verification_code = generate_verification_code("{email}{expires}".format(email=email, expires=expires))
-            response = app.test_client().get('/verify/{email}/{verification_code}/{expires}'.format(email=email, verification_code=verification_code, expires=expires), follow_redirects=True)
+            verification_code = generate_verification_code(
+                "{email}{expires}".format(email=email, expires=expires))
+            response = app.test_client().get('/verify/{email}/{verification_code}/{expires}'.format(
+                email=email, verification_code=verification_code, expires=expires), follow_redirects=True)
             self.assertIn(b'Verification complete!', response.data)
 
     def test_sending_confirmation_mail(self):
@@ -103,6 +116,7 @@ class TestVerify(unittest.TestCase):
             email = 'someone@example.com'
             response = send_signup_confirmation_mail(email)
             self.assertEqual(response.status_code, 401)
+
 
 class TestLogIn(unittest.TestCase):
     def setUp(self):
@@ -124,7 +138,7 @@ class TestLogIn(unittest.TestCase):
 
     def test_blank_password(self):
 
-        response = self.login(email = 'someone@example.com', password='')
+        response = self.login(email='someone@example.com', password='')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Password cannot be empty.', response.data)
 
@@ -143,4 +157,72 @@ class TestProfile(unittest.TestCase):
 
     def test_if_without_login_redirected_to_login_page(self):
         response = app.test_client().get('/profile')
-        self.assertIn(b'<a href="/login?next=mod_auth.profile">/login?next=mod_auth.profile</a>', response.data)
+        self.assertIn(
+            b'<a href="/login?next=mod_auth.profile">/login?next=mod_auth.profile</a>', response.data)
+
+
+def login_helper(client, email, password, submit='Login'):
+    with app.app_context():
+        admin_user = Users.query.filter(
+            Users.email == app.config['ADMIN_EMAIL']).first()
+
+        if admin_user is None:
+            admin_user = Users(email=app.config['ADMIN_EMAIL'],
+                               name=app.config['ADMIN_NAME'],
+                               password=app.config['ADMIN_PWD'],
+                               account_type=AccountType.admin)
+
+            db.session.add(admin_user)
+            db.session.commit()
+    return client.post('/login', data=dict(email=email,
+                                           password=password, submit=submit), follow_redirects=True)
+
+
+class TestUsersAdmin(unittest.TestCase):
+    def setUp(self):
+        app.config['WTF_CSRF_CHECK_DEFAULT'] = False
+        pass
+
+    def test_can_access_users_list_page(self):
+        client = app.test_client()
+        login_helper(
+            client, app.config['ADMIN_EMAIL'], app.config['ADMIN_PWD'])
+
+        response = client.get("/admin-dashboard/users")
+        self.assertEqual(response.status_code, 200)
+
+        self.assertIn(b'id="dataTable"', response.data)
+        self.assertIn(b'<th>Account Type</th>', response.data)
+
+    def test_if_without_login_redirected_to_login_page(self):
+        client = app.test_client()
+
+        response = client.get("/admin-dashboard/users")
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(
+            b'<a href="/login?next=mod_dashboard.user_list">/login?next=mod_dashboard.user_list</a>', response.data)
+
+
+class TestFilesAdmin(unittest.TestCase):
+    def setUp(self):
+        app.config['WTF_CSRF_ENABLED'] = False
+        pass
+
+    def test_can_access_uploaded_files_list_page(self):
+        client = app.test_client()
+        r = login_helper(
+            client, app.config['ADMIN_EMAIL'], app.config['ADMIN_PWD'])
+
+        response = client.get("/admin-dashboard/files")
+        self.assertEqual(response.status_code, 200)
+
+        self.assertIn(b'id="dataTable"', response.data)
+        self.assertIn(b'<th>User ID</th>', response.data)
+
+    def test_if_without_login_redirected_to_login_page(self):
+        client = app.test_client()
+
+        response = client.get("/admin-dashboard/files")
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(
+            b'<a href="/login?next=mod_dashboard.admin_uploaded_files">/login?next=mod_dashboard.admin_uploaded_files</a>', response.data)
