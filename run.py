77ed1de6a08156c782676362f1c082f6ffa46bba
alufers <alufers@wp.pl>
2018-10-26 21:07:01 +0200

    Added 404 and 500 page with test

--- run.py
+++ run.py
@@ -9,8 +9,8 @@ Link     : https://github.com/saurabhshri
 
 import os
 
-from flask import Flask, g, session
-
+from flask import Flask, g, session, render_template
+from template import LayoutHelper
 from database import db
 from logger import Logger
 from config_parser import general_config
@@ -228,6 +228,22 @@ def before_first_request():
     os.makedirs(os.path.dirname(os.path.join(app.config['LOGS_DIR'])), exist_ok=True)
     os.makedirs(os.path.dirname(os.path.join(app.config['OUTPUT_DIR'])), exist_ok=True)
 
+@app.errorhandler(404)
+def page_not_found_handler(e):
+    """
+    Handles the 404 error and displays a template.
+    """
+    layout = LayoutHelper(logged_in=False)
+    return render_template('404.html', layout=layout), 404
+
+
+@app.errorhandler(500)
+def internal_server_error_handler(e):
+    """
+    Handles the 500 error and displays a template.
+    """
+    layout = LayoutHelper(logged_in=False)
+    return render_template('500.html', layout=layout), 500
 
 if __name__ == '__main__':
     log.debug("Running App.")
