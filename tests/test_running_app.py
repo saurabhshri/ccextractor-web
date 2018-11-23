ef243064277efaf38d02f95667e2b57f80466bb7
T1duS <udit121sanghi@gmail.com>
2018-11-23 13:49:45 +0530

    Added a test

--- tests/test_running_app.py
+++ tests/test_running_app.py
@@ -9,6 +9,7 @@ Link     : https://github.com/saurabhshri
 import unittest, requests
 
 from run import app
+from mod_auth.models import AccountType, Users
 
 class TestRunningApp(unittest.TestCase):
     def create_app(self):
@@ -17,3 +18,10 @@ class TestRunningApp(unittest.TestCase):
     def test_flask_application_is_up_and_running(self):
         response = app.test_client().get('/')
         self.assertEqual(response.status_code, 302)
+
+    def test_admin_user_added(self):
+        """
+        Test that an admin user is created on initializing app
+        """  
+        user = Users.query.filter(Users.account_type == AccountType.admin).first()
+        self.assertNotEqual(user, None)
