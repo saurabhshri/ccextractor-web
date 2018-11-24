3c73ccb7fe0e02d5f710c2932199b199623f93ac
encadyma <encadyma@gmail.com>
2018-11-23 17:27:25 -0800

    Commit landing page (draft 1)

--- mod_landing/controller.py
+++ mod_landing/controller.py
@@ -3,15 +3,20 @@ from template import LayoutHelper
 
 mod_landing = Blueprint("mod_landing", __name__)
 
+from mod_landing.models import LandingPageStatistics
+
 @mod_landing.route('/')
 def index():
     layout = LayoutHelper(logged_in = False)
+
+    # Displaying statistics - might be insecure?
+    statistics = LandingPageStatistics()
 
     try:
         if g.user is not None:
             return redirect(url_for('mod_dashboard.dashboard'))
 
     except Exception as e:
-        return render_template("mod_landing/landing.html", layout = layout.get_entries())
+        return render_template("mod_landing/landing.html", layout = layout.get_entries(), statistics = statistics)
 
-    return render_template("mod_landing/landing.html", layout = layout.get_entries())
+    return render_template("mod_landing/landing.html", layout = layout.get_entries(), statistics = statistics)
---
+++ mod_landing/models.py
@@ -0,0 +1,21 @@
+"""
+ccextractor-web | models.py
+
+Author   : Saurabh Shrivastava
+Email    : saurabh.shrivastava54+ccextractorweb[at]gmail.com
+Link     : https://github.com/saurabhshri
+
+"""
+
+from mod_dashboard.models import ProcessQueue, ProcessStauts
+from mod_auth.models import Users
+
+class LandingPageStatistics():
+    user_count = 0
+    files_pending_count = 0
+    files_completed_count = 0
+
+    def __init__(self):
+        self.users = Users.query.all(),
+        self.files_pending =  ProcessQueue.query.filter((ProcessQueue.status == ProcessStauts.pending)).all(),
+        self.files_completed =  ProcessQueue.query.filter((ProcessQueue.status == ProcessStauts.completed)).all()
